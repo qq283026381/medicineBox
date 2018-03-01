aa7c831653a34203cd07be29bbdfaede319385fd
Vincent <283026381@qq.com>
2018-03-01 19:45:59 +0800

    添加对登录状况的简单判断（后续要使用JWT进行验证）；添加对当前页面的判断，从而调整导航栏的激活标签

--- res/js/controller/indexController.js
+++ res/js/controller/indexController.js
@@ -1,3 +1,18 @@
-medicineBox.controller('indexCtrl', ['$scope', function ($scope) {
-    $scope.test = 'index page';
+var login = localStorage.login;
+if (login === '' || typeof(login) === 'undefined') {
+    alert('请先登录');
+    window.location.href = 'login.html'
+}
+medicineBox.controller('indexCtrl', ['$scope', 'loginService', '$window', function ($scope, loginService, $window) {
+    if (($window.location.pathname.split('/')[2]) === 'main' || ($window.location.pathname.split('/')[2]) === '') {
+        $scope.currentPage = '';
+        sessionStorage.page = '';
+    }
+    $scope.currentPage = sessionStorage.page ? sessionStorage.page : '';
+    $scope.changeNavBar = function (item) {
+        console.log(item);
+        sessionStorage.page = item;
+        $scope.currentPage = item;
+        console.log($scope.currentPage);
+    }
 }]);
