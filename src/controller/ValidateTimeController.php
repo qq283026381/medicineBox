4918b9f545ee892c74d01c834f623bca574ba4db
Vincent <283026381@qq.com>
2018-03-06 14:37:25 +0800

    token过期验证方法，将ajax传入的token以及公钥进行JWT解密，对解密结果进行处理后返回前端

---
+++ src/controller/ValidateTimeController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vincent
+ * Date: 2018/3/5
+ * Time: 17:10
+ */
+require_once '../util/Tools.php';
+$tools = new Tools();
+$token = isset($_POST['token']) ? $_POST['token'] : '';
+$jti = isset($_POST['jti']) ? $_POST['jti'] : '';
+if ($token != '' && $jti != '') {
+    require_once '../util/JWT.php';
+    $jwt = new JWT();
+    $data = $jwt->decode($token, $jti);
+    $result = [];
+    switch ($data) {
+        case 1:
+            $result['result'] = 'token不完整';
+            break;
+        case 2:
+            $result['result'] = 'token加密算法不正确';
+            break;
+        case 3:
+            $result['result'] = '签名不正确';
+            break;
+        case 4:
+            $result['result'] = 'token创建时间有误';
+            break;
+        case 5:
+            $result['result'] = 'token过期';
+            break;
+        default:
+            $result['result'] = 'true';
+            break;
+    }
+    echo json_encode($result);
+}
