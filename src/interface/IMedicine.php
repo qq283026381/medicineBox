82d23bf2cc272ce1f1a9b1135f4adcfddf5daf04
Vincent <283026381@qq.com>
2018-03-27 18:41:21 +0800

    药品方法接口，包括根据用户ID获取全部药品方法和添加药品方法

---
+++ src/interface/IMedicine.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vincent
+ * Date: 2018/3/27
+ * Time: 13:54
+ */
+interface IMedicine{
+    public function getAllMedicine($user);
+    public function addMedicine($medicine);
+}
