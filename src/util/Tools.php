dff0c419a780699d276cd36e6321551f0e765705
Vincent <283026381@qq.com>
2018-03-29 14:02:03 +0800

    添加对权限判断方法和回滚登录页面的方法

--- src/util/Tools.php
+++ src/util/Tools.php
@@ -15,6 +15,44 @@ class Tools
 
     function setData($data)
     {
-        return json_encode($data,JSON_UNESCAPED_UNICODE);
+        return json_encode($data, JSON_UNESCAPED_UNICODE);
+    }
+
+    function checkAuthority()
+    {
+        $token = isset($_COOKIE['mbs']) ? $_COOKIE['mbs'] : '';
+        $jti = isset($_COOKIE['jti']) ? $_COOKIE['jti'] : '';
+        $result = 'false';
+        if ($token != '' && $jti != '') {
+            require_once 'JWT.php';
+            $jwt = new JWT();
+            $data = $jwt->decode($token, $jti);
+            switch ($data) {
+                case 1:
+                    $result = 'token不完整';
+                    break;
+                case 2:
+                    $result = 'token加密算法不正确';
+                    break;
+                case 3:
+                    $result = '签名不正确';
+                    break;
+                case 4:
+                    $result = 'token创建时间有误';
+                    break;
+                case 5:
+                    $result = 'token过期';
+                    break;
+                default:
+                    $result = 'true';
+                    break;
+            }
+        }
+        return $result;
+    }
+
+    function goBack()
+    {
+        echo "<script>alert('NO ACCESS !');window.location.href='../../login.html';</script>";
     }
 }
