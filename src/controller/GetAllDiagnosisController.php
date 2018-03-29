d733bb346c95bbfa7d1e8a669c948efb502a2c34
Vincent <283026381@qq.com>
2018-03-29 14:07:03 +0800

    添加对权限的判断

--- src/controller/GetAllDiagnosisController.php
+++ src/controller/GetAllDiagnosisController.php
@@ -9,25 +9,29 @@ require_once '../util/Tools.php';
 require_once '../implement/DiagnosisImpl.php';
 $diagnosis = new DiagnosisImpl();
 $tools = new Tools();
-$result = $diagnosis->getAllDiagnosis();
-$data = [];
-$names = [];
-$types = [];
-$content = [];
-while ($row = $result->fetch_assoc()) {
-    array_push($data, $row);
-    array_push($names, $row['diagnosis_name']);
-    array_push($types, $row['diagnosis_type']);
-}
-$types = array_merge(array_flip(array_flip($types)));
-for ($i = 0; $i < sizeof($types); $i++) {
-    $item = [];
-    for ($j = 0; $j < sizeof($data); $j++) {
-        if ($data[$j]['diagnosis_type'] == $types[$i]) {
-            array_push($item, $data[$j]['diagnosis_name']);
-        }
+if ($tools->checkAuthority() !== 'true') {
+    $tools->goBack();
+} else {
+    $result = $diagnosis->getAllDiagnosis();
+    $data = [];
+    $names = [];
+    $types = [];
+    $content = [];
+    while ($row = $result->fetch_assoc()) {
+        array_push($data, $row);
+        array_push($names, $row['diagnosis_name']);
+        array_push($types, $row['diagnosis_type']);
     }
-    $content[$i]['diagnosisType'] = $types[$i];
-    $content[$i]['diagnosisName'] = $item;
+    $types = array_merge(array_flip(array_flip($types)));
+    for ($i = 0; $i < sizeof($types); $i++) {
+        $item = [];
+        for ($j = 0; $j < sizeof($data); $j++) {
+            if ($data[$j]['diagnosis_type'] == $types[$i]) {
+                array_push($item, $data[$j]['diagnosis_name']);
+            }
+        }
+        $content[$i]['diagnosisType'] = $types[$i];
+        $content[$i]['diagnosisName'] = $item;
+    }
+    echo $tools->setData($content);
 }
-echo $tools->setData($content);
