d732ea8d70e954bf5bb6fc2ac9664570f4d7e2af
Vincent <283026381@qq.com>
2018-03-29 18:25:07 +0800

    添加删除药品接口

--- src/interface/IMedicine.php
+++ src/interface/IMedicine.php
@@ -1,12 +1,18 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: Vincent
  * Date: 2018/3/27
  * Time: 13:54
  */
-interface IMedicine{
+interface IMedicine
+{
     public function getAllMedicine($user);
+
     public function addMedicine($medicine);
+
     public function updateMedicine($medicine);
+
+    public function deleteMedicine($userId, $medicineId);
 }
