a604600be6854f55a5758d6c9f01c1b55104a72e
Vincent <283026381@qq.com>
2018-03-29 18:25:47 +0800

    添加获取用户id方法。从cookie中读取token，解码后返回

--- src/util/Tools.php
+++ src/util/Tools.php
@@ -55,4 +55,37 @@ class Tools
     {
         echo "<script>alert('NO ACCESS !');window.location.href='../../login.html';</script>";
     }
+
+    function getUserId()
+    {
+        $token = isset($_COOKIE['mbs']) ? $_COOKIE['mbs'] : '';
+        $jti = isset($_COOKIE['jti']) ? $_COOKIE['jti'] : '';
+        $result = 'false';
+        if ($token != '' && $jti != '') {
+            require_once 'JWT.php';
+            $jwt = new JWT();
+            $payload = $jwt->decode($token, $jti);
+            switch ($payload) {
+                case 1:
+                    $result = 'token不完整';
+                    break;
+                case 2:
+                    $result = 'token加密算法不正确';
+                    break;
+                case 3:
+                    $result = '签名不正确';
+                    break;
+                case 4:
+                    $result = 'token创建时间有误';
+                    break;
+                case 5:
+                    $result = 'token过期';
+                    break;
+                default:
+                    $result = $payload['aud'];
+                    break;
+            }
+        }
+        return $result;
+    }
 }
