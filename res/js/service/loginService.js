5c891a896d6439b1a9f49bbf483cc74f4e343129
Vincent <283026381@qq.com>
2018-03-29 13:45:32 +0800

    添加cookies和http注入 新增获取cookie方法 判断登录方法改为http服务从后台获取data 注销方法改为后台清除cookie

--- res/js/service/loginService.js
+++ res/js/service/loginService.js
@@ -1,45 +1,25 @@
-medicineBox.service('loginService', [function () {
+medicineBox.service('loginService', ['$cookies', '$http', function ($cookies, $http) {
+    this.getCookie = function (item) {
+        return $cookies.get(item);
+    };
     this.checkTime = function () {
-        var token = localStorage.getItem('authorization') || '';
-        var jti = localStorage.getItem('jti') || '';
-        $.ajax({
-            url: 'ValidateTime.do',
-            method: 'post',
-            async: false,
-            data: {
-                'token': token,
-                'jti': jti
-            },
-            dataType: 'json',
-            success: function (data) {
-                if (data.result !== 'true') {
-                    alert('登录失效，请重新登陆');
-                    window.location.href = 'login.html';
-                }
+        $http.post('ValidateTime.do').then(function (t) {
+            if (t.data.result !== 'true') {
+                alert('登录失效，请重新登陆');
+                window.location.href = 'login.html';
             }
-        });
+        })
     };
     this.checkLoginStatus = function () {
-        var token = localStorage.getItem('authorization') || '';
-        var jti = localStorage.getItem('jti') || '';
-        $.ajax({
-            url: 'ValidateTime.do',
-            method: 'post',
-            async: false,
-            data: {
-                'token': token,
-                'jti': jti
-            },
-            dataType: 'json',
-            success: function (data) {
-                if (data.result === 'true') {
-                    window.location.href = 'main';
-                }
+        $http.post('ValidateTime.do').then(function (t) {
+            if (t.data.result === 'true') {
+                window.location.href = 'main';
+            }else{
+                logout();
             }
-        });
+        })
     };
     this.logout = function () {
-        localStorage.removeItem('authorization');
-        localStorage.removeItem('jti');
+        $http.post('Logout.do');
     }
 }]);
