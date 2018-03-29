a85c011026f2e24b99505850d0cd0f99630163d0
Vincent <283026381@qq.com>
2018-03-29 18:26:43 +0800

    删除药品控制器。对用户身份进行验证，通过后从cookie中获取用户id，从angularJS传来的数据中获取药品ID，将药品从数据库中删除

---
+++ src/controller/DeleteMedicineController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vincent
+ * Date: 2018/3/29
+ * Time: 16:53
+ */
+require_once '../util/Tools.php';
+require_once '../implement/MedicineImpl.php';
+$tools = new Tools();
+if ($tools->checkAuthority() !== 'true') {
+    $tools->goBack();
+} else {
+    $data=$tools->getData();
+    $userId=$tools->getUserId();
+    if($userId!='false'){
+        if(gettype($userId)=='integer'){
+            $medicineId=$data['id'];
+            $operation=new MedicineImpl();
+            $result=$operation->deleteMedicine($userId,$medicineId);
+            echo $tools->setData($result);
+        }
+    }
+}
