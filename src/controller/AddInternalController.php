c538d4907323609bcdb14064fa35a2cb8fdb59ba
Vincent <283026381@qq.com>
2018-04-07 20:48:58 +0800

    添加内科体检记录方法。对用户权限进行判断，通过后，获取从angular传来的数据，对内科体检记录进行添加，添加成功后，将体检记录表中内科ID栏的数据更新

---
+++ src/controller/AddInternalController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vincent
+ * Date: 2018/4/7
+ * Time: 19:09
+ */
+require_once '../util/Tools.php';
+$tools = new Tools();
+if ($tools->checkAuthority() !== 'true') {
+    $tools->goBack();
+} else {
+    $data = $tools->getData();
+    $userId = $tools->getUserId();
+    require_once '../model/InternalModel.php';
+    require_once '../implement/InternalImpl.php';
+    $internal = new InternalModel($userId, $data['height'], $data['weight'], $data['BMI'], $data['bloodPressure'], $data['heartRate'], $data['heart'], $data['noise'], $data['liver'], $data['spleen'], $data['lung'], $data['internalSum']);
+    $operation = new InternalImpl();
+    $result = $operation->addInternal($internal);
+    if($result['result']){
+        require_once '../implement/RecordingImpl.php';
+        $recording=new RecordingImpl();
+        $recording->updateInternal($result['number'],$data['recordingId']);
+    }
+    echo $tools->setData($result);
+}
