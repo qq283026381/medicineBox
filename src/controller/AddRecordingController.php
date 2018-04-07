919d321cfaed28ff228b2a5f96398c5a2ac9afba
Vincent <283026381@qq.com>
2018-04-07 20:49:15 +0800

    添加体检记录控制器。

---
+++ src/controller/AddRecordingController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vincent
+ * Date: 2018/4/7
+ * Time: 14:35
+ */
+require_once '../util/Tools.php';
+require_once '../model/RecordingModel.php';
+require_once '../implement/RecordingImpl.php';
+$tools = new Tools();
+if ($tools->checkAuthority() !== 'true') {
+    $tools->goBack();
+} else {
+    $data = $tools->getData();
+    $userId = $tools->getUserId();
+    if ($userId != 'false') {
+        if (gettype($userId) == 'integer') {
+            $recording = new RecordingModel($userId, $data['name'], $data['gender'], $data['time']);
+            $operation = new RecordingImpl();
+            $result = $operation->addRecording($recording);
+            echo $tools->setData($result);
+        }
+    }
+}
