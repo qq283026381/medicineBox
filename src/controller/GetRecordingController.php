dc0800a9b8f82a51ade2258e2ec1c366614de01f
Vincent <283026381@qq.com>
2018-04-07 20:50:20 +0800

    根据体检记录ID获取体检记录控制器

---
+++ src/controller/GetRecordingController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vincent
+ * Date: 2018/4/7
+ * Time: 20:32
+ */
+require_once '../util/Tools.php';
+require_once '../implement/RecordingImpl.php';
+$tools = new Tools();
+if ($tools->checkAuthority() !== 'true') {
+    $tools->goBack();
+} else {
+    $data = $tools->getData();
+    $operation = new RecordingImpl();
+    $result = $operation->getRecording($data['recordingId']);
+    if ($result->num_rows > 0) {
+        $content = [];
+        while ($row = $result->fetch_assoc()) {
+            $content = array(
+                'gender' => $row['gender'],
+                'internal' => $row['internal_id'],
+                'surgical' => $row['surgical_id'],
+                'ophthalmology' => $row['ophthalmology_id'],
+                'gynecology' => $row['gynecology_id'],
+                'infrared_mammogram' => $row['infrared_mammogram_id'],
+                'B_scan' => $row['B_scan_id'],
+                'bone_density' => $row['bone_density_id'],
+                'ECG' => $row['ECG_id'],
+                'X_ray' => $row['X_ray_id'],
+                'biochemical' => $row['biochemical_id'],
+                'blood_routine' => $row['blood_routine_id'],
+                'urine_routine' => $row['urine_routine_id']
+            );
+        }
+        echo $tools->setData($content);
+    }
+}
