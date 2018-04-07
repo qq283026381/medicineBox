08635704bd6834b37f06c022a0f9d5600bf7f09a
Vincent <283026381@qq.com>
2018-04-07 20:52:12 +0800

    体检记录实现，包括添加体检记录方法，获取全部体检记录方法，更新体检记录的内科ID方法，根据体检ID获取体检记录方法

---
+++ src/implement/RecordingImpl.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vincent
+ * Date: 2018/4/7
+ * Time: 14:30
+ */
+require_once '../util/Mysql.php';
+require_once '../interface/IRecording.php';
+
+class RecordingImpl implements IRecording
+{
+    private $mysql;
+    private $conn;
+
+    /**
+     * RecordingImpl constructor.
+     */
+    public function __construct()
+    {
+        $this->mysql = new Mysql();
+        $this->conn = $this->mysql->getConnection();
+    }
+
+    public function addRecording($recording)
+    {
+        $query = 'INSERT INTO recording(user_id,name,gender,time) VALUES(?,?,?,?)';
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param('isss', $recording->getUserId(), $recording->getName(), $recording->getGender(), $recording->getTime());
+        $result = $stmt->execute();
+        $number = $stmt->insert_id;
+        $this->mysql->closeConnection();
+        return array('result' => $result, 'number' => $number);
+    }
+
+    public function getAllRecordings($userId)
+    {
+        $query = 'SELECT * FROM recording WHERE user_id=?';
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param('i', $userId);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $this->mysql->closeConnection();
+        return $result;
+    }
+
+    public function updateInternal($internalId, $recordingId)
+    {
+        $query = 'UPDATE recording SET internal_id=? WHERE recording_id=?';
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param('ii', $internalId, $recordingId);
+        $result = $stmt->execute();
+        return array('result' => $result);
+    }
+
+    public function getRecording($recordingId)
+    {
+        $query = 'SELECT * FROM recording WHERE recording_id=?';
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param('i', $recordingId);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $this->mysql->closeConnection();
+        return $result;
+    }
+}
