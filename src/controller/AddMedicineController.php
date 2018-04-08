6534e078960d6d360cb65d80e64e49f06c56fec8
Vincent <283026381@qq.com>
2018-04-08 13:00:58 +0800

    添加对

--- src/controller/AddMedicineController.php
+++ src/controller/AddMedicineController.php
@@ -15,25 +15,29 @@ if ($tools->checkAuthority() !== 'true') {
     $userId = $tools->getUserId();
     if ($userId != 'false' && gettype($userId) == 'integer') {
         $data = $tools->getData();
-        $name = $data['name'];
-        $productionDate = $data['productionDate'];
-        $validity = $data['validity'];
-        $time = $data['time'];
-        $number = $data['number'];
-        $date = explode('-', $productionDate);
-        $date[1] += $validity;
-        if ($date[1] > 12) {
-            $date[0] += intval($date[1] / 12);
-            $date[1] %= 12;
-            if ($date[1] > 0 && $date[1] < 10) {
-                $date[1] = '0' . $date[1];
+        if ($tools->checkData($data)) {
+            $name = $data['name'];
+            $productionDate = $data['productionDate'];
+            $validity = $data['validity'];
+            $time = $data['time'];
+            $number = $data['number'];
+            $date = explode('-', $productionDate);
+            $date[1] += $validity;
+            if ($date[1] > 12) {
+                $date[0] += intval($date[1] / 12);
+                $date[1] %= 12;
+                if ($date[1] > 0 && $date[1] < 10) {
+                    $date[1] = '0' . $date[1];
+                }
             }
+            $deadline = implode('-', $date);
+            $time = implode(';', $time);
+            $medicine = new MedicineModel('', $userId, $name, $productionDate, $validity, $deadline, $time, $number);
+            $operation = new MedicineImpl();
+            $result = $operation->addMedicine($medicine);
+            echo $tools->setData($result);
+        } else {
+            echo $tools->setData(array('result' => false));
         }
-        $deadline = implode('-', $date);
-        $time = implode(';', $time);
-        $medicine = new MedicineModel('', $userId, $name, $productionDate, $validity, $deadline, $time, $number);
-        $operation = new MedicineImpl();
-        $result = $operation->addMedicine($medicine);
-        echo $tools->setData($result);
     }
 }
