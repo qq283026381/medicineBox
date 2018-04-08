cdb5b4a2db0e418f5f0833aa0d725f61a1987a49
Vincent <283026381@qq.com>
2018-04-08 13:04:04 +0800

    添加对传入数据的判空

--- src/controller/GetRecordingController.php
+++ src/controller/GetRecordingController.php
@@ -12,27 +12,31 @@ if ($tools->checkAuthority() !== 'true') {
     $tools->goBack();
 } else {
     $data = $tools->getData();
-    $operation = new RecordingImpl();
-    $result = $operation->getRecording($data['recordingId']);
-    if ($result->num_rows > 0) {
-        $content = [];
-        while ($row = $result->fetch_assoc()) {
-            $content = array(
-                'gender' => $row['gender'],
-                'internal' => $row['internal_id'],
-                'surgical' => $row['surgical_id'],
-                'ophthalmology' => $row['ophthalmology_id'],
-                'gynecology' => $row['gynecology_id'],
-                'infrared_mammogram' => $row['infrared_mammogram_id'],
-                'B_scan' => $row['B_scan_id'],
-                'bone_density' => $row['bone_density_id'],
-                'ECG' => $row['ECG_id'],
-                'X_ray' => $row['X_ray_id'],
-                'biochemical' => $row['biochemical_id'],
-                'blood_routine' => $row['blood_routine_id'],
-                'urine_routine' => $row['urine_routine_id']
-            );
+    if ($tools->checkData($data)) {
+        $operation = new RecordingImpl();
+        $result = $operation->getRecording($data['recordingId']);
+        if ($result->num_rows > 0) {
+            $content = array('result' => true);
+            while ($row = $result->fetch_assoc()) {
+                $content['data'] = array(
+                    'gender' => $row['gender'],
+                    'internal' => $row['internal_id'],
+                    'surgical' => $row['surgical_id'],
+                    'ophthalmology' => $row['ophthalmology_id'],
+                    'gynecology' => $row['gynecology_id'],
+                    'infrared_mammogram' => $row['infrared_mammogram_id'],
+                    'B_scan' => $row['B_scan_id'],
+                    'bone_density' => $row['bone_density_id'],
+                    'ECG' => $row['ECG_id'],
+                    'X_ray' => $row['X_ray_id'],
+                    'biochemical' => $row['biochemical_id'],
+                    'blood_routine' => $row['blood_routine_id'],
+                    'urine_routine' => $row['urine_routine_id']
+                );
+            }
+            echo $tools->setData($content);
         }
-        echo $tools->setData($content);
+    } else {
+        echo $tools->setData(array('result' => false));
     }
 }
