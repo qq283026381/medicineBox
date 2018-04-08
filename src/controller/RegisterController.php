4377ef3c00012d3c51506c7c9393cdde0c03b417
Vincent <283026381@qq.com>
2018-04-08 13:04:25 +0800

    添加对传入数据的判空

--- src/controller/RegisterController.php
+++ src/controller/RegisterController.php
@@ -10,13 +10,17 @@ require_once '../model/UserModel.php';
 require_once '../implement/LoginImpl.php';
 $tools = new Tools();
 $data = $tools->getData();
-$register = new LoginImpl();
-$check = $register->checkUser($data['name']);
-if ($check->num_rows > 0) {
-    echo $tools->setData(array('result'=>'用户名已存在'));
+if ($tools->checkData($data)) {
+    $register = new LoginImpl();
+    $check = $register->checkUser($data['name']);
+    if ($check->num_rows > 0) {
+        echo $tools->setData(array('result' => false, 'data' => '用户名已经存在'));
+    } else {
+        $user = new UserModel($data['name'], $data['pwd'], $data['email'], $data['phone']);
+        $result = $register->register($user);
+        echo $tools->setData($result);
+    }
 } else {
-    $user = new UserModel($data['name'], $data['pwd'], $data['email'], $data['phone']);
-    $result = $register->register($user);
-    echo $tools->setData($result);
+    echo $tools->setData(array('result' => false, 'data' => '注册失败'));
 }
 
