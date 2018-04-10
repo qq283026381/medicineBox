999b30dd21e5c15aa3e34adc0de4015e31d4b86c
Vincent <283026381@qq.com>
2018-04-10 14:40:48 +0800

    添加对获取数据方法的限制，当体检记录表里没有记录体检项时，不调用对应获取数据方法

--- res/js/controller/searchRecordingController.js
+++ res/js/controller/searchRecordingController.js
@@ -32,112 +32,124 @@ medicineBox.controller('searchRecordingCtrl', ['$scope', '$http', 'toaster', '$f
             }
         });
         var getInternalDetail = function () {
-            $http.post('GetInternal.do', {'internal': $scope.info.internal}).then(function (t) {
-                if (t.data.result) {
-                    $scope.internal = t.data.data;
-                } else {
-                    toaster.pop('error', '', '内科数据获取失败，请联系管理员');
-                }
-            })
+            if ($scope.info.internal !== -1)
+                $http.post('GetInternal.do', {'internal': $scope.info.internal}).then(function (t) {
+                    if (t.data.result) {
+                        $scope.internal = t.data.data;
+                    } else {
+                        toaster.pop('error', '', '内科数据获取失败，请联系管理员');
+                    }
+                })
         };
         var getSurgicalDetail = function () {
-            $http.post('GetSurgical.do', {'surgical': $scope.info.surgical}).then(function (t) {
-                if (t.data.result) {
-                    $scope.surgical = t.data.data;
-                } else {
-                    toaster.pop('error', '', '外科数据获取失败，请联系管理员');
-                }
-            })
+            if ($scope.info.surgical !== -1)
+                $http.post('GetSurgical.do', {'surgical': $scope.info.surgical}).then(function (t) {
+                    if (t.data.result) {
+                        $scope.surgical = t.data.data;
+                    } else {
+                        toaster.pop('error', '', '外科数据获取失败，请联系管理员');
+                    }
+                })
         };
         var getOphthalmologyDetail = function () {
-            $http.post('GetOphthalmology.do', {'ophthalmology': $scope.info.ophthalmology}).then(function (t) {
-                if (t.data.result) {
-                    $scope.ophthalmology = t.data.data;
-                } else {
-                    toaster.pop('error', '', '眼科数据获取失败，请联系管理员');
-                }
-            })
+            if ($scope.info.ophthalmology !== -1)
+                $http.post('GetOphthalmology.do', {'ophthalmology': $scope.info.ophthalmology}).then(function (t) {
+                    if (t.data.result) {
+                        $scope.ophthalmology = t.data.data;
+                    } else {
+                        toaster.pop('error', '', '眼科数据获取失败，请联系管理员');
+                    }
+                })
         };
         var getGynecologyDetail = function () {
-            $http.post('GetGynecology.do', {'gynecology': $scope.info.gynecology}).then(function (t) {
-                if (t.data.result) {
-                    $scope.gynecology = t.data.data;
-                } else {
-                    toaster.pop('error', '', '妇科数据获取失败，请联系管理员');
-                }
-            })
+            if ($scope.info.gynecology !== -1)
+                $http.post('GetGynecology.do', {'gynecology': $scope.info.gynecology}).then(function (t) {
+                    if (t.data.result) {
+                        $scope.gynecology = t.data.data;
+                    } else {
+                        toaster.pop('error', '', '妇科数据获取失败，请联系管理员');
+                    }
+                })
         };
         var getInfraredMammogramDetail = function () {
-            $http.post('GetInfraredMammogram.do', {'infraredMammogram': $scope.info.infrared_mammogram}).then(function (t) {
-                if (t.data.result) {
-                    $scope.infraredMammogram = t.data.data;
-                } else {
-                    toaster.pop('error', '', '红外线乳透数据获取失败，请联系管理员');
-                }
-            })
+            if ($scope.info.infrared_mammogram !== -1)
+                $http.post('GetInfraredMammogram.do', {'infraredMammogram': $scope.info.infrared_mammogram}).then(function (t) {
+                    if (t.data.result) {
+                        $scope.infraredMammogram = t.data.data;
+                    } else {
+                        toaster.pop('error', '', '红外线乳透数据获取失败，请联系管理员');
+                    }
+                })
         };
         var getBScanDetail = function () {
-            $http.post('GetBScan.do', {'BScan': $scope.info.B_scan}).then(function (t) {
-                if (t.data.result) {
-                    $scope.BScan = t.data.data;
-                } else {
-                    toaster.pop('error', '', 'B超数据获取失败，请联系管理员');
-                }
-            })
+            if ($scope.info.B_scan !== -1)
+                $http.post('GetBScan.do', {'BScan': $scope.info.B_scan}).then(function (t) {
+                    if (t.data.result) {
+                        $scope.BScan = t.data.data;
+                    } else {
+                        toaster.pop('error', '', 'B超数据获取失败，请联系管理员');
+                    }
+                })
         };
         var getBoneDensityDetail = function () {
-            $http.post('GetBoneDensity.do', {'boneDensity': $scope.info.bone_density}).then(function (t) {
-                if (t.data.result) {
-                    $scope.boneDensity = t.data.data;
-                } else {
-                    toaster.pop('error', '', '骨密度数据获取失败，请联系管理员');
-                }
-            })
+            if ($scope.info.bone_density !== -1)
+                $http.post('GetBoneDensity.do', {'boneDensity': $scope.info.bone_density}).then(function (t) {
+                    if (t.data.result) {
+                        $scope.boneDensity = t.data.data;
+                    } else {
+                        toaster.pop('error', '', '骨密度数据获取失败，请联系管理员');
+                    }
+                })
         };
         var getECGDetail = function () {
-            $http.post('GetECG.do', {'ECG': $scope.info.ECG}).then(function (t) {
-                if (t.data.result) {
-                    $scope.ECG = t.data.data;
-                } else {
-                    toaster.pop('error', '', '心电图数据获取失败，请联系管理员');
-                }
-            })
+            if ($scope.info.ECG !== -1)
+                $http.post('GetECG.do', {'ECG': $scope.info.ECG}).then(function (t) {
+                    if (t.data.result) {
+                        $scope.ECG = t.data.data;
+                    } else {
+                        toaster.pop('error', '', '心电图数据获取失败，请联系管理员');
+                    }
+                })
         };
         var getXRayDetail = function () {
-            $http.post('GetXRay.do', {'XRay': $scope.info.X_ray}).then(function (t) {
-                if (t.data.result) {
-                    $scope.XRay = t.data.data;
-                } else {
-                    toaster.pop('error', '', 'X射线数据获取失败，请联系管理员');
-                }
-            })
+            if ($scope.info.X_ray !== -1)
+                $http.post('GetXRay.do', {'XRay': $scope.info.X_ray}).then(function (t) {
+                    if (t.data.result) {
+                        $scope.XRay = t.data.data;
+                    } else {
+                        toaster.pop('error', '', 'X射线数据获取失败，请联系管理员');
+                    }
+                })
         };
         var getBiochemicalDetail = function () {
-            $http.post('GetBiochemical.do', {'biochemical': $scope.info.biochemical}).then(function (t) {
-                if (t.data.result) {
-                    $scope.biochemical = t.data.data;
-                } else {
-                    toaster.pop('error', '', '生化检查数据获取失败，请联系管理员');
-                }
-            })
+            if ($scope.info.biochemical !== -1)
+                $http.post('GetBiochemical.do', {'biochemical': $scope.info.biochemical}).then(function (t) {
+                    if (t.data.result) {
+                        $scope.biochemical = t.data.data;
+                    } else {
+                        toaster.pop('error', '', '生化检查数据获取失败，请联系管理员');
+                    }
+                })
         };
         var getBloodRoutineDetail = function () {
-            $http.post('GetBloodRoutine.do', {'bloodRoutine': $scope.info.blood_routine}).then(function (t) {
-                if (t.data.result) {
-                    $scope.bloodRoutine = t.data.data;
-                } else {
-                    toaster.pop('error', '', '血常规数据获取失败，请联系管理员');
-                }
-            })
+            if ($scope.info.blood_routine !== -1)
+                $http.post('GetBloodRoutine.do', {'bloodRoutine': $scope.info.blood_routine}).then(function (t) {
+                    if (t.data.result) {
+                        $scope.bloodRoutine = t.data.data;
+                    } else {
+                        toaster.pop('error', '', '血常规数据获取失败，请联系管理员');
+                    }
+                })
         };
         var getUrineRoutineDetail = function () {
-            $http.post('GetUrineRoutine.do', {'urineRoutine': $scope.info.urine_routine}).then(function (t) {
-                if (t.data.result) {
-                    $scope.urineRoutine = t.data.data;
-                } else {
-                    toaster.pop('error', '', '尿常规数据获取失败，请联系管理员');
-                }
-            })
+            if ($scope.info.urine_routine !== -1)
+                $http.post('GetUrineRoutine.do', {'urineRoutine': $scope.info.urine_routine}).then(function (t) {
+                    if (t.data.result) {
+                        $scope.urineRoutine = t.data.data;
+                    } else {
+                        toaster.pop('error', '', '尿常规数据获取失败，请联系管理员');
+                    }
+                })
         };
         $scope.info = angular.fromJson(item);
         $scope.initInfo = function () {
