999ba7b98c2a6dfa281f99178d06b6b2e5a08c51
Vincent <283026381@qq.com>
2018-04-16 16:04:13 +0800

    修改登录验证方法逻辑，添加获取验证码方法，添加注册成功后的方法

--- res/js/controller/loginController.js
+++ res/js/controller/loginController.js
@@ -132,99 +132,115 @@ medicineBox.controller('loginCtrl', ['$scope', '$http', 'toaster', '$interval', 
                 $scope.register.error.name = true;
                 toaster.pop('error', '', '请输入用户名');
             } else {
-                if(!$scope.register.error.nameFormat){
+                if (!$scope.register.error.nameFormat) {
                     $scope.register.error.name = false;
                 }
                 registerNameWatcher();
-                /***
-                 * 监听用户是否输入设置密码
-                 * 当用户输入设置密码后，取消监听
-                 */
-                var registerPwdWatcher = $scope.$watch('register.pwd', function (pwd) {
-                    if (pwd === '') {
-                        $scope.register.error.pwd = true;
-                        toaster.pop('error', '', '请输入密码');
-                    } else {
-                        $scope.register.error.pwd = false;
-                        registerPwdWatcher();
-                        /***
-                         * 监听用户是否输入确认密码
-                         * 当用户输入确认密码并且与设置密码相同后，取消监听
-                         */
-                        var registerPwdRepeatWatcher = $scope.$watch('register.pwdRepeat', function (pwdRepeat) {
-                            if (pwdRepeat === '') {
-                                $scope.register.error.pwdRepeat = true;
-                                toaster.pop('error', '', '请确认密码');
-                            } else {
-                                if (!$scope.register.error.correct) {
-                                    $scope.register.error.pwdRepeat = false;
-                                }
-                                registerPwdRepeatWatcher();
-                                /***
-                                 * 监听用户是否输入邮箱
-                                 * 当用户输入格式合法的邮箱后，取消监听
-                                 */
-                                var registerEmailWatcher = $scope.$watch('register.email', function (email) {
-                                    if (email === '') {
-                                        $scope.register.error.email = true;
-                                        toaster.pop('error', '', '请输入邮箱');
-                                    } else {
-                                        if (!$scope.register.error.emailFormat) {
-                                            $scope.register.error.email = false;
-                                        }
-                                        registerEmailWatcher();
-                                        /***
-                                         * 监听用户是否输入手机号
-                                         * 当用户输入手机号后，取消监听
-                                         */
-                                        var registerPhoneWatcher = $scope.$watch('register.phone', function (phone) {
-                                            if (phone === '') {
-                                                $scope.register.error.phone = true;
-                                                toaster.pop('error', '', '请输入手机号');
-                                            } else {
-                                                $scope.register.error.phone = false;
-                                                registerPhoneWatcher();
-                                                /***
-                                                 * 监听用户是否输入验证码
-                                                 * 当用户输入验证码后，取消监听
-                                                 */
-                                                var registerCodeWatcher = $scope.$watch('register.code', function (code) {
-                                                    if (code === '') {
-                                                        $scope.register.error.code = true;
-                                                        toaster.pop('error', '', '请输入验证码');
-                                                    } else {
-                                                        $scope.register.error.code = false;
-                                                        registerCodeWatcher();
-                                                        /***
-                                                         * 当以上监听器全部取消后，判断两次输入密码是否相同、邮箱格式是否合法以及手机号格式输入是否合法
-                                                         * 若密码相同、邮箱格式合法且手机号输入合法，则取消对确认密码、设置邮箱和手机号输入的监听，并对用户信息进行注册
-                                                         * TODO 需要对密码进行加密处理，尚未完成后台接口，暂滞空
-                                                         */
-                                                        if (!$scope.register.error.correct &&
-                                                            !$scope.register.error.emailFormat &&
-                                                            !$scope.register.error.phoneFormat &&
-                                                            !$scope.register.error.nameFormat) {
-                                                            pwdRepeatWatcher();
-                                                            emailFormatWatcher();
-                                                            phoneFormatWatcher();
-                                                            nameFormatWatcher();
-                                                            console.log($scope.register);
-                                                            $http.post('Register.do', $scope.register).then(function (t) {
-                                                                console.log(t);
-                                                            })
-                                                        }
-                                                    }
-                                                })
-                                            }
-                                        })
-                                    }
-                                })
-                            }
-                        })
-                    }
-                })
             }
         });
+        /***
+         * 监听用户是否输入设置密码
+         * 当用户输入设置密码后，取消监听
+         */
+        var registerPwdWatcher = $scope.$watch('register.pwd', function (pwd) {
+            if (pwd === '') {
+                $scope.register.error.pwd = true;
+                toaster.pop('error', '', '请输入密码');
+            } else {
+                $scope.register.error.pwd = false;
+                registerPwdWatcher();
+            }
+        });
+        /***
+         * 监听用户是否输入确认密码
+         * 当用户输入确认密码并且与设置密码相同后，取消监听
+         */
+        var registerPwdRepeatWatcher = $scope.$watch('register.pwdRepeat', function (pwdRepeat) {
+            if (pwdRepeat === '') {
+                $scope.register.error.pwdRepeat = true;
+                toaster.pop('error', '', '请确认密码');
+            } else {
+                if (!$scope.register.error.correct) {
+                    $scope.register.error.pwdRepeat = false;
+                }
+                registerPwdRepeatWatcher();
+            }
+        });
+        /***
+         * 监听用户是否输入邮箱
+         * 当用户输入格式合法的邮箱后，取消监听
+         */
+        var registerEmailWatcher = $scope.$watch('register.email', function (email) {
+            if (email === '') {
+                $scope.register.error.email = true;
+                toaster.pop('error', '', '请输入邮箱');
+            } else {
+                if (!$scope.register.error.emailFormat) {
+                    $scope.register.error.email = false;
+                }
+                registerEmailWatcher();
+            }
+        });
+        /***
+         * 监听用户是否输入手机号
+         * 当用户输入手机号后，取消监听
+         */
+        var registerPhoneWatcher = $scope.$watch('register.phone', function (phone) {
+            if (phone === '') {
+                $scope.register.error.phone = true;
+                toaster.pop('error', '', '请输入手机号');
+            } else {
+                $scope.register.error.phone = false;
+                registerPhoneWatcher();
+            }
+        });
+        /***
+         * 监听用户是否输入验证码
+         * 当用户输入验证码后，取消监听
+         */
+        var registerCodeWatcher = $scope.$watch('register.code', function (code) {
+            if (code === '') {
+                $scope.register.error.code = true;
+                toaster.pop('error', '', '请输入验证码');
+            } else {
+                $scope.register.error.code = false;
+                registerCodeWatcher();
+            }
+        });
+        /***
+         * 当以上监听器全部取消后，判断两次输入密码是否相同、邮箱格式是否合法以及手机号格式输入是否合法
+         * 若密码相同、邮箱格式合法且手机号输入合法，则取消对确认密码、设置邮箱和手机号输入的监听，并对用户信息进行注册
+         * TODO 需要对密码进行加密处理，尚未完成后台接口，暂滞空
+         */
+        if (!$scope.register.error.correct &&
+            !$scope.register.error.emailFormat &&
+            !$scope.register.error.phoneFormat &&
+            !$scope.register.error.nameFormat) {
+            pwdRepeatWatcher();
+            emailFormatWatcher();
+            phoneFormatWatcher();
+            nameFormatWatcher();
+
+            $scope.register.pwd = md5($scope.register.pwd);
+            $scope.register.pwdRepeat = md5($scope.register.pwdRepeat);
+            console.log($scope.register);
+            $http.post('Register.do', $scope.register).then(function (t) {
+                if (t.data.result) {
+                    toaster.pop('success', '', '注册成功,1s后跳转');
+                    setTimeout(function () {
+                        $http.post('Login.do', {
+                            'name': $scope.register.name,
+                            'pwd': $scope.register.pwd
+                        }).then(function (t) {
+                            if (t.data.result)
+                                $window.location.href = 'main';
+                        });
+                    }, 1000)
+                } else {
+                    toaster.pop('error', '注册失败', t.data.data);
+                }
+            })
+        }
     };
     /***
      * 获取验证码方法
@@ -246,7 +262,12 @@ medicineBox.controller('loginCtrl', ['$scope', '$http', 'toaster', '$interval', 
                 } else {
                     $scope.codeInfo = --deadline + 's后重新获取';
                 }
-            }, 1000, deadline)
+            }, 1000, deadline);
+            $http.post('SendMessage.do', {'method': 'register', 'phone': $scope.register.phone}).then(function (t) {
+                if (!t.data.result) {
+                    toaster.pop('error', '', '获取验证码失败');
+                }
+            })
         } else {
             $scope.register.error.phone = true;
             toaster.pop('error', '', '请先输入手机号');
