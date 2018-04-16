bc29a08e00c17beb8ce542aaa086409a90e5f08c
Vincent <283026381@qq.com>
2018-04-16 16:10:05 +0800

    获取验证码后，将验证码和携带的唯一ID存放在cookie中，注册时从cookie中获取验证码和ID进行验证，通过后进行注册

--- src/controller/RegisterController.php
+++ src/controller/RegisterController.php
@@ -9,18 +9,32 @@ require_once '../util/Tools.php';
 require_once '../model/UserModel.php';
 require_once '../implement/LoginImpl.php';
 $tools = new Tools();
-$data = $tools->getData();
-if ($tools->checkData($data)) {
-    $register = new LoginImpl();
-    $check = $register->checkUser($data['name']);
-    if ($check->num_rows > 0) {
-        echo $tools->setData(array('result' => false, 'data' => '用户名已经存在'));
+$message = (array)json_decode(base64_decode($_COOKIE['mbsco']));
+if ($message['uid'] === 'new' && isset($message['code'])) {
+    $code = $message['code'];
+    $data = $tools->getData();
+    if ($tools->checkData($data)) {
+        if ($code == $data['code']) {
+            $register = new LoginImpl();
+            $check = $register->checkUser($data['name']);
+            if ($check->num_rows > 0) {
+                setcookie('mbsco', '', time() - 3600);
+                echo $tools->setData(array('result' => false, 'data' => '用户名已经存在'));
+            } else {
+                $user = new UserModel($data['name'], $data['pwd'], $data['email'], $data['phone']);
+                $register = new LoginImpl();
+                $result = $register->register($user);
+                if ($result['result']) {
+                    setcookie('mbsco', '', time() - 3600);
+                }
+                echo $tools->setData($result);
+            }
+        } else {
+            echo $tools->setData(array('result' => false, 'data' => '验证码有误'));
+        }
     } else {
-        $user = new UserModel($data['name'], $data['pwd'], $data['email'], $data['phone']);
-        $result = $register->register($user);
-        echo $tools->setData($result);
+        echo $tools->setData(array('result' => false, 'data' => '注册失败'));
     }
-} else {
-    echo $tools->setData(array('result' => false, 'data' => '注册失败'));
 }
+
 
