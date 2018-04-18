a959c092269dfd062cbc10f25a03db021a341828
Vincent <283026381@qq.com>
2018-04-18 16:17:35 +0800

    药效方法接口

---
+++ src/interface/IEfficacy.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vincent
+ * Date: 2018/4/18
+ * Time: 12:48
+ */
+
+interface IEfficacy
+{
+    public function getAllEfficacy();
+
+    public function getEfficacyImg($id);
+}
